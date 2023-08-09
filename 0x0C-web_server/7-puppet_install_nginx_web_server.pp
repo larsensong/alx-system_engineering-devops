1f0f70b8375058561d7d9631cf6f4cd5caaf90a3
Larsen Song <larsenmulamba@gmail.com>
2023-08-09 10:04:46 +0000

    fix: Redirection

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,33 @@
-# Script that installs and configures Nginx
-exec {'update':
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get -y update',
+# Puppet manifest to configure nginx server
+
+$command_path = ['/usr/bin/', '/usr/sbin', '/bin']
+
+exec { 'update_package_repo':
+  provider => 'shell',
+  path     => $command_path,
+  command  => 'sudo apt -y update',
 }
 
-exec {'install':
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get -y install nginx',
+exec { 'install_nginx':
+  provider => 'shell',
+  path     => $command_path,
+  command  => 'sudo apt -y install nginx'
 }
 
-exec {'echo_html':
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
+exec { 'home_page':
+  provider => 'shell',
+  path     => $command_path,
+  command  => 'sudo echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html'
 }
 
-exec {'sed_config':
-  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
+exec { 'redirection':
+  provider => 'shell',
+  path     => $command_path,
+  command  => 'sudo sed -i "s@server_name _;@server_name _;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;@g" /etc/nginx/sites-available/default'
 }
 
-exec {'start':
-  command  => 'sudo service nginx start',
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
+exec { 'nginx_start':
+  provider => 'shell',
+  path     => $command_path,
+  command  => 'sudo service nginx start'
 }
